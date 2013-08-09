6a1f77fe98e7d7f7aafe3b3a0548d66395b65d2d
Ray Powell <rpowell1@uchicago.edu>
2013-08-09 10:51:37 -0500

    Ran pep8 across cloud_report.py. #2

--- cloud_report.py
+++ cloud_report.py
@@ -6,14 +6,15 @@ import ConfigParser
 import pprint
 from datetime import datetime, timedelta
 
+
 def weekbegend(year, week):
     """
     Calcul du premier et du dernier jour de la semaine ISO
-    Used from example at http://stackoverflow.com/questions/396913/in-python-how-do-i-find-the-date-of-the-first-monday-of-a-given-week
+    Used from example at http://stackoverflow.com/questions/
+    396913/in-python-how-do-i-find-the-date-of-the-first-monday-of-a-given-week
     Week is from Monday to Sunday night
-
     """
-    d = date(year, 1, 1)    
+    d = date(year, 1, 1)
     delta_days = d.isoweekday() - 1
     delta_weeks = week
     if year == d.isocalendar()[0]:
@@ -22,7 +23,7 @@ def weekbegend(year, week):
     delta = timedelta(days=-delta_days, weeks=delta_weeks)
     weekbeg = d + delta
     # delta2 for the end of the week
-    delta2 = timedelta(days=6-delta_days, weeks=delta_weeks)
+    delta2 = timedelta(days=6 - delta_days, weeks=delta_weeks)
     weekend = d + delta2
     return weekbeg, weekend
 
@@ -34,12 +35,12 @@ if __name__ == "__main__":
     email = False
     tenant_reporting = False
     sf = False
-    
+
     #Load in the CLI flags
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "", ["start=", "end=", 
-            "cloud=","thismonth","lastmonth", "thisweek","lastweek","now",
-            "noprint","email","tenants","sf"])
+        opts, args = getopt.getopt(sys.argv[1:], "", ["start=", "end=",
+            "cloud=", "thismonth", "lastmonth", "thisweek", "lastweek", "now",
+            "noprint", "email", "tenants", "sf"])
     except getopt.GetoptError:
         sys.stderr.write("ERROR: Getopt\n")
         sys.exit(2)
@@ -53,24 +54,26 @@ if __name__ == "__main__":
         elif opt in ("--thismonth"):
             now = datetime.now()
             start_date = now.strftime("%Y-%m-01 00:00:00")
-            end_date = (now.replace(month=now.month+1, day=1) - timedelta(days=1)).strftime("%Y-%m-%d 23:59:59")
+            end_date = (now.replace(month=now.month + 1, day=1) -
+                timedelta(days=1)).strftime("%Y-%m-%d 23:59:59")
         elif opt in ("--lastmonth"):
             now = datetime.now()
             date = now.replace(day=1) - timedelta(days=1)
             start_date = date.strftime("%Y-%m-01 00:00:00")
-            end_date = (date.replace(month=date.month+1, day=1) - timedelta(days=1)).strftime("%Y-%m-%d 23:59:59")
+            end_date = (date.replace(month=date.month + 1, day=1) -
+                timedelta(days=1)).strftime("%Y-%m-%d 23:59:59")
         elif opt in ("--thisweek"):
             now = datetime.now()
-            year = int( now.strftime("%Y") )
-            week = int( now.strftime("%U") )+1
-            weekbeg, weekend = weekbegend( year, week)
+            year = int(now.strftime("%Y"))
+            week = int(now.strftime("%U")) + 1
+            weekbeg, weekend = weekbegend(year, week)
             start_date = weekbeg.strftime("%Y-%m-%d 00:00:00")
             end_date = weekend.strftime("%Y-%m-%d 23:59:59")
         elif opt in ("--lastweek"):
             now = datetime.now()
-            year = int( now.strftime("%Y") )
-            week = int( now.strftime("%U") )
-            weekbeg, weekend = weekbegend( year, week)
+            year = int(now.strftime("%Y"))
+            week = int(now.strftime("%U"))
+            weekbeg, weekend = weekbegend(year, week)
             start_date = weekbeg.strftime("%Y-%m-%d 00:00:00")
             end_date = weekend.strftime("%Y-%m-%d 23:59:59")
         elif opt in ("--now"):
@@ -86,15 +89,11 @@ if __name__ == "__main__":
             tenant_reporting = True
         elif opt in ("--sf"):
             sf = True
-        
-        
 
     #initialize the clase with credentials
     nova_user_reports = NovaUserReporting(".settings")
     nova_user_reports.load_stats(start_date=start_date, end_date=end_date)
     nova_user_reports.gen_csv()
-
-
 
     if noprint is False:
         nova_user_reports.print_csv()
@@ -104,5 +103,3 @@ if __name__ == "__main__":
 
     if sf is True:
         nova_user_reports.push_to_sf()
-
-
