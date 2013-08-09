bc54743a4fec08887309926bd165843ee5e1641d
Ray Powell <rpowell1@uchicago.edu>
2013-08-09 11:36:42 -0500

    Pep8 pollgluster.py #2

--- pollgluster/pollgluster.py
+++ pollgluster/pollgluster.py
@@ -1,4 +1,4 @@
-from subprocess import check_output,call
+from subprocess import check_output, call
 
 from sqlalchemy import create_engine, insert, text
 from sqlalchemy.exc import SQLAlchemyError
@@ -17,38 +17,38 @@ from unitconversion import UnitConversion
 
 
 class PollGluster:
-    def __init__ (self,vol_name="USER-HOME", config_file=".settings"):
+    def __init__(self, vol_name="USER-HOME", config_file=".settings"):
         """Polls gluster for quotas and save into dict"""
-        self.settings={}
+        self.settings = {}
 
         #read in settings
         Config = ConfigParser.ConfigParser()
         Config.read(config_file)
-        sections =  Config.sections()
+        sections = Config.sections()
         for section in sections:
             options = Config.options(section)
             for option in options:
                 try:
                     self.settings[option] = Config.get(section, option)
                 except:
-                    print("exception on [%s] %s!" % section,option)
+                    print "exception on [%s] %s!" % (section, option)
 
-        self.gluster_cmd = [ "/usr/sbin/gluster", "volume", "quota", vol_name, "list"]
+        self.gluster_cmd = ["/usr/sbin/gluster", "volume", "quota",
+            vol_name, "list"]
         self.now_time = datetime.now(tz=pytz.timezone('UTC'))
         self.dus = {}
         self.metadata = MetaData()
-        self.du_table = Table( self.settings['cloud'], self.metadata,
+        self.du_table = Table(self.settings['cloud'], self.metadata,
                 Column('date', DateTime),
                 Column('path', Text),
                 Column('value', Float),
-        )   
-        
+        )
 
     def get_gluster_output(self):
         try:
-            raw_output = check_output( self.gluster_cmd )
+            raw_output = check_output(self.gluster_cmd)
 
-        except: 
+        except:
             sys.stderr.write("ERROR:Gluster Call Failed\n")
             sys.exit(1)
 
@@ -63,15 +63,14 @@ class PollGluster:
                     sys.stderr.write("ERROR: IndexError on %s\n" % (entry))
                     pass
 
-
     def print_du(self):
         """Print the du for each path"""
-        for key,value in self.dus.items():
+        for key, value in self.dus.items():
             print "Path: %s Value: %s " % (key, value)
 
-    def db_connect(self,db):
+    def db_connect(self, db):
         try:
-            dsn = "mysql://%s:%s@%s/%s" % ( self.settings['uid'],
+            dsn = "mysql://%s:%s@%s/%s" % (self.settings['uid'],
                 self.settings['pwd'], self.settings['server'], db)
             engine = create_engine(dsn)
             return engine.connect()
@@ -86,18 +85,26 @@ class PollGluster:
         insert = []
         unitconverter = UnitConversion()
         for path, value in self.dus.items():
-            insert.append({'date': self.now_time, 'path': path, 'value': unitconverter.human2bytes(value)})
+            insert.append({'date': self.now_time,
+                'path': path,
+                'value': unitconverter.human2bytes(value)
+            })
         conn.execute(self.du_table.insert(), insert)
 
     def get_average_du(self, start_date=None, end_date=None, path=None):
         """ Retrieve paths """
         if start_date is  None or end_date is None:
-            sys.stderr.write("ERROR: Start and End Dates no specified in get_average_du")
+            sys.stderr.write(
+                "ERROR: Start and End Dates no specified in get_average_du")
             sys.exit(1)
 
         self.set_time_range(start_date=start_date, end_date=end_date)
 
-        my_query = "SELECT AVG(value) FROM %s where ( date >= '%s' and date <= '%s' ) and path = '%s'" % ( self.settings['cloud'], self.start_time.strftime(self.settings['timeformat']), self.cieling_time.strftime(self.settings['timeformat']), path )
+        my_query = "SELECT AVG(value) FROM %s where ( date >= '%s' and date <= '%s' ) and path = '%s'" % (
+            self.settings['cloud'],
+            self.start_time.strftime(self.settings['timeformat']),
+            self.cieling_time.strftime(self.settings['timeformat']),
+            path)
 
         try:
             conn = self.db_connect(self.settings['glusterdb'])
@@ -109,7 +116,7 @@ class PollGluster:
             sys.stderr.write("Erroring querying the databases\n")
             sys.exit(1)
 
-    def set_time_range(self,start_date=None, end_date=None):
+    def set_time_range(self, start_date=None, end_date=None):
         """ Set the time range """
 
         # Set time zone and localize inputs
