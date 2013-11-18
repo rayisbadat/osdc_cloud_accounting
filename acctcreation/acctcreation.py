c0a5d8e7bb4aa8051840a6282902a3bc4b0561aa
raymond powell <rpowell1@kl05-compute-1.lac.uchicago.edu>
2013-11-17 18:29:26 -0600

    snapup

---
+++ acctcreation/acctcreation.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+from salesforceocc import SalesForceOCC
+
+
+if __name__ == "__main__":
+	pass
---
+++ bin/sfaccts.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+from salesforceocc import SalesForceOCC
+import ConfigParser
+
+
+if __name__ == "__main__":
+    sfocc = SalesForceOCC()
+    #read in settings
+    Config = ConfigParser.ConfigParser()
+    Config.read(".settings")
+    sections = ['general','salesforceocc']
+    settings = {}
+    for section in sections:
+        options = Config.options(section)
+        for option in options:
+            try:
+                settings[option] = Config.get(section, option)
+            except:
+                sys.stderr.write("exception on [%s] %s!" % section, option)
+
+
+    sfocc.login(username=settings['sfusername'], password=settings['sfpassword'])
+    contacts = sfocc.get_contacts_from_campaign(campaign_name=settings['cloud'],  statuses=["Approved User", "Application Pending"])
+    sfocc.print_approved_users_csv(campaign_name=settings['cloud'], contacts=contacts)
