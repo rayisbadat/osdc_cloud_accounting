4997d65d0c6684592b4e21a763b18328924b1bc1
Ray Powell <rpowell1@uchicago.edu>
2014-02-27 14:25:33 -0600

    Made an example folder of the config files that need configureing

---
+++ examples/settings.sh
@@ -0,0 +1,10 @@
+export BASEDN=""
+export ADMINCN="cn=admin,$BASEDN"
+export LDAP_SECRET="/etc/pam_ldap.secret"
+
+export ADC_SID=""
+
+export GLUSTER_VOL="USER-HOME"
+export DISK_QUOTA="1TB"
+
+export ACCTCREATION_SSHKEY='~/id_rsa'
