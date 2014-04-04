5d9458b450b9fe248c1c3759ea96b2a027097ee7
Ray Powell <rpowell1@uchicago.edu>
2014-04-04 14:38:06 -0500

    the update script now will let you just get the current values

--- bin/update_nova_floating_ips_quota.sh
+++ bin/update_nova_floating_ips_quota.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 USERNAME=${1}
-CORES=${2}
+IPS=${2}
 
 if [ -e /etc/osdc_cloud_accounting/admin_auth ]
 then
@@ -21,10 +21,12 @@ fi
 
 if [ -z "$USERNAME" ]
 then
-    echo "Usage: $0 USERNAME [cores]"
-	exit 1
+    echo "Usage: $0 USERNAME [ips]"
+    exit 1
 fi
+
 tennant_id=$(/usr/bin/keystone tenant-list 2>/dev/null | grep $USERNAME | perl -ne 'm/\|\s(\S+)\s/ && print "$1"')
+
 
 if [ "$tennant_id" == "" ]
 then
@@ -32,24 +34,10 @@ then
     exit 1
 fi
 
-
-if [ -z "$CORES" ]
+if [ -z "$IPS" ]
 then
     nova-manage account quota --project=${tennant_id}
     exit 0
 fi
 
-ram=$((2048*$CORES))
-instances=${CORES}
-
-if [ "$tennant_id" == "" ]
-then
-    echo "$0 Error: No user/tennant found for $USERNAME"
-    exit 1
-fi
-
-nova-manage account quota --project=${tennant_id} --key=cores --value=$CORES &>/dev/null
-nova-manage account quota --project=${tennant_id} --key=ram --value=$ram &>/dev/null
-nova-manage account quota --project=${tennant_id} --key=instances --value=$instances &>/dev/null
-nova-manage account quota --project=${tennant_id} --key=fixed_ips --value=$instances &>/dev/null
-nova-manage account quota --project=${tennant_id} --key=floating_ips --value=0 &>/dev/null
+nova-manage account quota --project=${tennant_id} --key=floating_ips --value=$IPS #&>/dev/null
