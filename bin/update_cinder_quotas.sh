f14ce80b2a8d7d7b2fa9d2226ef20774f2672afb
Enoch Root <admin@opensciencedatacloud.org>
2014-04-17 16:41:35 -0500

    I created a cinder quota updater

---
+++ bin/update_cinder_quotas.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+set -e
+set -u
+
+if [ -e /etc/osdc_cloud_accounting/admin_auth ]
+then
+    source  /etc/osdc_cloud_accounting/admin_auth
+else
+    echo "$0 Error: can not locate /etc/osdc_cloud_accounting/admin_auth"
+    exit 1
+fi
+
+if [ -e /etc/osdc_cloud_accounting/settings.sh ]
+then
+    source  /etc/osdc_cloud_accounting/settings.sh
+else
+    echo "$0 Error: can not locate /etc/osdc_cloud_accounting/settings "
+    exit 1
+fi
+
+while getopts ":t:v:g:s:" opt; do
+  case ${opt} in
+    t)
+	tennant_id=$(/usr/bin/keystone tenant-list 2>/dev/null | grep $OPTARG | perl -ne 'm/\|\s(\S+)\s/ && print "$1"')
+
+	if [ "$tennant_id" == "" ]
+	then
+    		echo "$0 Error: No user/tennant found for $USERNAME"
+    		exit 1
+	fi
+	;;
+    v)
+        cinder quota-update --volumes $OPTARG $tennant_id
+      ;;
+    g)
+        cinder quota-update --gigabytes $OPTARG $tennant_id
+      ;;
+    s)
+        cinder quota-update --snapshots $OPTARG $tennant_id
+      ;;
+    ?)
+      echo "Usage: $0 USERNAME -t TENANT [-v NUM_VOLUMES] [-g SIZE_IN_GB] [-s NUM_SNAPSHOTS]"
+      exit 1
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument." >&2
+      exit 1
+      ;;
+  esac
+done
+
+if [ $OPTIND -eq 2 ] && [ "$tennant_id" != "" ]
+then 
+	cinder quota-show $tennant_id
+fi
