12206e72326fc070c196c3a359a5b21440b0ee04
Ray Powell <rpowell1@uchicago.edu>
2014-08-06 23:38:31 -0500

    I have offically mangled this script, but it auto assigns the phsid membership roles for acls

--- bin/sf_acct_create.py
+++ bin/sf_acct_create.py
@@ -8,6 +8,8 @@ import getopt
 import pprint
 import csv
 import  os
+from sqlalchemy import create_engine, select, text
+from sqlalchemy.exc import SQLAlchemyError
 
 def tenant_exist(tenant):
     """ Check if tenant exists """
@@ -134,10 +136,89 @@ def lock_unapproved_users(approved_members=None, starting_uid=1500, printdebug=N
         except subprocess.CalledProcessError, e:
             sys.stderr.write("Error: %s user %s\n" % (operation,username))
             sys.stderr.write("%s\n" % e.output)
-        
+
+
+def get_tenant_members(tenant=None):
+    """ Return list of users in a tenant, the client API wants the user we run as to be a member of every tenant we query :( """
+    query = """select c.name as User_Name
+        from keystone.project a
+            join keystone.assignment b
+                on b.target_id = a.id
+            join keystone.user c
+                on b.actor_id = c.id
+        where a.name='%s';
+        """ % tenant
+
+    members = set()
+
+    try:
+        conn = db_connect(settings['accounts']['novadb'])
+        s = text(query)
+        results = conn.execute(s)
+    except SQLAlchemyError as e:
+        sys.stderr.write("ERROR-NUR: Erroring querying the databases: %s\n" % (e) )
+        sys.exit(1)
+
+
+    for row in results:
+        try:
+            members.add(row[0])
+        except KeyError:
+            pass
+        except ValueError:
+            pass
+
+    return members
+    
+def db_connect(db):
+    try:
+        dsn = "mysql://%s:%s@%s/%s" % (settings['accounts']['db_user'],
+            settings['accounts']['db_passwd'], settings['accounts']['db_server'], db)
+        engine = create_engine(dsn)
+        return engine.connect()
+
+    except SQLAlchemyError, e:
+        sys.stderr.write("Error %s" % (e))
+        sys.exit(1)
 
 
         
+def remove_member_from_tenant(tenant=None, users=None, role="_member_"):
+    """  Remove users from the tenant """
+    for user in users:
+        print "INFO: Removing user %s from tenant %s" % (user, tenant)
+        cmd = [
+            'keystone',
+            'user-role-remove',
+            "--user=%s"%(user),
+            "--tenant=%s"%(tenant),
+            "--role=%s"%(role),
+        ]
+        try:
+            result = subprocess.check_call( cmd )
+        except subprocess.CalledProcessError, e:
+            sys.stderr.write("Error: Removing  user %s to tenant %s\n" % (username,tenant) )
+            sys.stderr.write("%s\n" % e.output)
+    
+
+def add_member_to_tenant(tenant=None, users=None, role="_member_"):
+    """ Add users to the tenant """  
+    for user in users:
+        print "INFO: Adding user %s to tenant %s" % (user, tenant)
+        cmd = [
+            'keystone',
+            'user-role-add',
+            "--user=%s"%(user),
+            "--tenant=%s"%(tenant),
+            "--role=%s"%(role),
+        ]
+        try:
+            result = subprocess.check_call( cmd )
+        except subprocess.CalledProcessError, e:
+            sys.stderr.write("Error: Adding  user %s to tenant %s\n" % (username,tenant) )
+            sys.stderr.write("%s\n" % e.output)
+        
+
 
 if __name__ == "__main__":
 
@@ -145,7 +226,7 @@ if __name__ == "__main__":
     run = True
     printdebug = False
     nih_file = False
-    approved_members = []    
+    approved_members = set()
 
     try:
         opts, args = getopt.getopt(sys.argv[1:], "", ["debug", "norun", "nihfile="])
@@ -165,7 +246,7 @@ if __name__ == "__main__":
     #read in settings
     Config = ConfigParser.ConfigParser()
     Config.read("/etc/osdc_cloud_accounting/settings.py")
-    sections = ['general','salesforceocc','tukey']
+    sections = ['general','salesforceocc','tukey', 'accounts']
     settings = {}
     for section in sections:
         options = Config.options(section)
@@ -175,6 +256,20 @@ if __name__ == "__main__":
                 settings[section][option] = Config.get(section, option)
             except:
                 sys.stderr.write("exception on [%s] %s!" % section, option)
+
+    #One offs:
+    try:
+        settings['accounts']['managed_tenants']=settings['accounts']['managed_tenants'].split(',')
+        managed_tenants={}
+        # Loop through managed tenants in config and initialize
+        #Setting to set() since we need to do intersections and differences
+        for managed_tenant in settings['accounts']['managed_tenants']:
+            managed_tenants[managed_tenant]=set()
+            if printdebug:
+                print "DEBUG: created managed tenant: %s" % (managed_tenant)
+                pprint.pprint( managed_tenants )
+    except:
+        pass
 
     #Load up a list of the users in SF that are approved for this cloud
     sfocc.login(username=settings['salesforceocc']['sfusername'], password=settings['salesforceocc']['sfpassword'])
@@ -192,15 +287,30 @@ if __name__ == "__main__":
     #login is the name we care about.
     #phsid is also important eventually
     if nih_file:
+        
+        if printdebug:
+            print "DEBUG: maned_tenants:" 
+            pprint.pprint(settings['accounts']['managed_tenants'])
+
         try:
             with open(nih_file, 'r') as handle:
                 reader = csv.DictReader(handle, ['user_name', 'login', 'authority', 'role', 'email', 'phone',' status', 'phsid', 'permission_set', 'created', 'updated', 'expires', 'downloader_for'])
+                # Loop through csv and load the user info
+                # we are loading the phsids as an array
                 for row in reader:
-                    nih_approved_users[row['login'].upper()] = row['phsid']
-                    if printdebug:
-                        pprint.pprint( nih_approved_users[row['login'].upper()]  )
-        except NameError:
-            pass
+                    try:
+                        if type(nih_approved_users[row['login'].upper()]) is not list:
+                            nih_approved_users[row['login'].upper()]=[]
+                    except KeyError:
+                        nih_approved_users[row['login'].upper()]=[]
+                    #add in phsid to array
+                    nih_approved_users[row['login'].upper()].append(row['phsid'])
+
+                    #Create a list of who belongs to what managed tenant for later parsing
+                    if row['phsid'].split(".")[0] in settings['accounts']['managed_tenants']:
+                        managed_tenants[row['phsid'].split(".")[0]].add(row['login'].upper())
+
+                        
         except IOError as e:
             sys.exit('file %s not found: %s' % (nih_file, e))
         except csv.Error as e:
@@ -209,6 +319,7 @@ if __name__ == "__main__":
 
     #Loop through list of members and run the bash scripts that will create the account
     #FIXME: Most of this stuff could be moved into the python, just simpler not to
+    print "Creating New Users:"
     for username, fields in members_list.items():
         #Nih style changes....i am doing this wrong
         if nih_file:
@@ -223,7 +334,7 @@ if __name__ == "__main__":
             print "DEBUG: Username from SF = %s" % (username) 
 
         #At some point we need to disable inactive users
-        approved_members.append(username)
+        approved_members.add(username)
         
         try:
             user_exists = pwd.getpwnam(username)
@@ -244,7 +355,7 @@ if __name__ == "__main__":
                         #Can not create a user with out an existing tenant
                         sys.stderr.write("ERROR: New User %s has no existing tenant %s \n" % (username, fields['tenant']) )
                         continue
-
+        
             #Create the user
             if username:
                 print "INFO: Creating users %s" % username
@@ -264,4 +375,41 @@ if __name__ == "__main__":
         starting_uid=settings['general']['starting_uid']
     except KeyError:
         starting_uid=1500
-    lock_unapproved_users(approved_members=approved_members,starting_uid=starting_uid,printdebug=printdebug,) 
+    print "Disabling Users:"
+    #lock_unapproved_users(approved_members=approved_members,starting_uid=starting_uid,printdebug=printdebug,) 
+
+    #Fix the tenants for manged groups
+    #csv_tenant_members = 
+    #approved_tenant_members
+    #current_tenant_members =  currently on system as memebrs
+    #valid_tenant_memebers = they are the ones we are keeping in the tennat
+    #invalid_tenant_members = users we need to remove from a tenant
+    #additional_tenant_members = users we need to add
+    for tenant_name, csv_tenant_members in managed_tenants.items():
+        #Intersection of CSV and SalesForce
+        approved_tenant_members = csv_tenant_members.intersection( approved_members )
+        #Current users in tenant
+        current_tenant_members = get_tenant_members(tenant=tenant_name)
+        #Keep these user in tenant, they are still valid
+        valid_tenant_members = current_tenant_members.intersection(approved_tenant_members)
+        #We need to remove these users from the tenant
+        invalid_tenant_members = current_tenant_members.difference(valid_tenant_members)
+        #We need to add these users to the tenant
+        additional_tenant_members = approved_tenant_members.difference( valid_tenant_members ) 
+
+        if printdebug:
+            print tenant_name
+            pprint.pprint( approved_tenant_members ) 
+            pprint.pprint( valid_tenant_members ) 
+            pprint.pprint( invalid_tenant_members ) 
+            pprint.pprint( additional_tenant_members ) 
+       
+        print "Adjusting tenant membership" 
+        remove_member_from_tenant(tenant=tenant_name, users=invalid_tenant_members)
+        add_member_to_tenant(tenant=tenant_name, users=additional_tenant_members)
+        
+        
+        
+        
+        
+
