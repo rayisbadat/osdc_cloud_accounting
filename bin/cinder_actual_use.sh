319b971702b639effbbcf55ee88f2efc057d4a8f
Ray Powell <admin@opensciencedatacloud.org>
2014-12-17 10:56:00 -0600

    moooo

--- bin/cinder_actual_use.sh
+++ bin/cinder_actual_use.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
+source /etc/osdc_cloud_accounting/admin_auth
 cinder_allocated=$(cinder list --all_tenants | tr -s " " | cut -d"|" -f5 | grep -v "+" | perl -lan -e '$core = $1 if m|(\d+)|;' -e '$sum+=$core;' -e'print "+$core=\n$sum";'| tail -n1)
 actually_used=$(ceph df | perl -ne 'm|volumes\s+\d+\s+(\S+)| && print "$1\n"')
 
-echo "Currently ${cinder_allocated}GB of Cinder Blocks are provisioned with $actually_used of data written to them"
+echo "Currently ${cinder_allocated} GB of Cinder Blocks are allocated,  with ${actually_used} GB of data written to them"
