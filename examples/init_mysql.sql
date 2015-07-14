66480cf91d2d0bc0565f943a29c78578292953b2
root <root@griffin.opensciencedatacloud.org>
2015-07-14 21:01:11 +0000

    fixed init mysql example

--- examples/init_mysql.sql
+++ examples/init_mysql.sql
@@ -2,8 +2,7 @@ CREATE DATABASE storage;
 USE storage;
 CREATE TABLE `$TABLE_NAME` (   `date` datetime DEFAULT NULL,   `path` text,   `value` float DEFAULT NULL );
 GRANT USAGE ON *.* TO '$REPORTING_USERNAME'@'%' IDENTIFIED BY '$PASSWORD';
-GRANT ALL PRIVILEGES ON `storage_use`.* TO '$REPORTING_USERNAME'@'%';
+GRANT ALL PRIVILEGES ON `storage`.* TO '$REPORTING_USERNAME'@'%';
 GRANT SELECT ON `nova`.`instance_types` TO '$REPORTING_USERNAME'@'%';
 GRANT SELECT ON `nova`.`instances` TO '$REPORTING_USERNAME'@'%';
-
-CREATE TABLE `PDCv2_object` (   `date` datetime DEFAULT NULL,   `username` text, `tenant_name` text, `value` float DEFAULT NULL );
+CREATE TABLE `${CLOUD}_object` (   `date` datetime DEFAULT NULL,   `username` text, `tenant_name` text, `value` float DEFAULT NULL );
