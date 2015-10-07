11161664bfa97fa5bc7e5b2539ca3b6a561c92cc
root <root@igsb.opensciencedatacloud.org>
2015-10-07 09:22:35 -0500

    updated install guide

--- examples/settings.sh
+++ examples/settings.sh
@@ -8,3 +8,10 @@ export GLUSTER_VOL="USER-HOME"
 export DISK_QUOTA="1TB"
 
 export ACCTCREATION_SSHKEY='~/id_rsa'
+
+
+export s3creds_db_name="storage"
+export s3creds_db_table="$CLOUDNAME_s3_keys"
+export s3creds_db_server="mysql-$CLOUDNAME.fqdn.tld"
+export s3creds_db_user="reporting"
+export s3creds_db_passwd="hunter2"
