5a7673ab937df5f4022d54c47dee2975d079cb81
root <root@griffin.opensciencedatacloud.org>
2016-03-04 11:52:54 -0600

    modified to work with no keystone'ed ceph s3 finally

--- bin/get_tenant_cephs3_stats.sh
+++ bin/get_tenant_cephs3_stats.sh
@@ -11,8 +11,12 @@ set -u
 
 tenant_name=$1
 
-uuid=$( keystone tenant-get $tenant_name 2>/dev/null| perl -n -e 'm/id\s+\|\s+(\S+)/ && print "$1\n"' )
-radosgw-admin user stats --uid=$uuid
+radosgw-admin user stats --uid=$tenant_name 2>/dev/null
+if [ "$?" != "0" ]
+then
+    uuid=$( keystone tenant-get $tenant_name 2>/dev/null| perl -n -e 'm/id\s+\|\s+(\S+)/ && print "$1\n"' )
+    radosgw-admin user stats --uid=$uuid 2>/dev/null
+fi
 
 
 
