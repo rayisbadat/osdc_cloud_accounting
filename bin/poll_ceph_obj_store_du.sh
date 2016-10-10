1453f36355ad2db69c9e2a2bf14a3f77d40a66bb
root <root@griffin.opensciencedatacloud.org>
2016-10-10 13:58:42 -0500

    additional tweak so the poller uses new gflag in commit 6d7e8532ae4b69d0216276e3598691567638da11

--- bin/poll_ceph_obj_store_du.sh
+++ bin/poll_ceph_obj_store_du.sh
@@ -5,8 +5,15 @@ source /usr/local/src/.SFACCT/bin/activate
 cd /usr/local/src/osdc_cloud_accounting/repcephosdu
 
 
-for username in $(keystone user-list | tr -s " "| cut -f3 -d"|"  | grep -Ev " admin | neutron | nova | cinder |glance | ceilometer |\+| name")
+for username in $(keystone user-list 2>/dev/null | tr -s " "| cut -f3 -d"|"  | grep -Ev " admin | neutron | nova | cinder |glance | ceilometer |\+| name")
 do
     novarc=$(getent passwd ${username} | cut -f6 -d":")/.novarc
-    ./repcephosdu.py --novarc=${novarc}  --update
+    if [ -e /etc/osdc_cloud_accounting/project_overrides ]
+    then
+        ./repcephosdu.py --novarc=${novarc}  --update --project_override_file /etc/osdc_cloud_accounting/project_overrides;
+    else
+        ./repcephosdu.py --novarc=${novarc}  --update
+    fi
+
+
 done
