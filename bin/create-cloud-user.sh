7e99a267d47c780d5b4b86c5bedc25bb413b0763
root <root@griffin.opensciencedatacloud.org>
2017-03-07 15:00:00 -0600

    made the new/extra stuff in .novarc only create if set in settings.sh

--- bin/create-cloud-user.sh
+++ bin/create-cloud-user.sh
@@ -48,9 +48,9 @@ echo "export OS_TENANT_NAME=$PROJECT" >> $credential_file
 echo "export OS_USERNAME=$USERNAME" >> $credential_file
 echo "export OS_PASSWORD=$PASSWORD" >> $credential_file
 echo "export OS_AUTH_URL=\"${IDENTITY_URL}/\"" >> $credential_file
-echo "export OS_IDENTITY_API_VERSION=$IDENTITY_API_VERSION" >> $credential_file
-echo "export OS_IMAGE_API_VERSION=$IMAGE_API_VERSION" >> $credential_file
+[ "$IDENTITY_API_VERSION" != "" ] && echo "export OS_IDENTITY_API_VERSION=$IDENTITY_API_VERSION" >> $credential_file
+[ "$IMAGE_API_VERSION" != "" ] && echo "export OS_IMAGE_API_VERSION=$IMAGE_API_VERSION" >> $credential_file
 #Defaults
-echo 'export OS_DOMAIN_NAME="default"' >> $credential_file
-echo 'export OS_PROJECT_DOMAIN_NAME="default"' >> $credential_file
-echo 'export OS_USER_DOMAIN_NAME="default"' >> $credential_file
+[ "$DOMAIN" != "" ] && echo 'export OS_DOMAIN_NAME="default"' >> $credential_file
+[ "$DOMAIN" != "" ] && echo 'export OS_PROJECT_DOMAIN_NAME="default"' >> $credential_file
+[ "$DOMAIN" != "" ] && echo 'export OS_USER_DOMAIN_NAME="default"' >> $credential_file
